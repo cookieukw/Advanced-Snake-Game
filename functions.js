369121b9b3192e144fce85ce0e33556bcbc15500
Cookie :3 <65344982+cookieukw@users.noreply.github.com>
2022-06-01 12:57:46 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,6 +3,8 @@ if(!canvasSupport()){
   clearScreen()
   window.stop()
 }
+
+
 const canvas = document.getElementById('game');
 const cookie = canvas.getContext('2d');
 const block = 16;
@@ -290,4 +292,12 @@ function clearScreen(){
   paragrafo.appendChild(texto);
   paragrafo.setAttribute('id','txt');
   document.body.appendChild(paragrafo);
+}
+
+function isMobile(){
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+return true
+} else {
+return false;
+}
 }
